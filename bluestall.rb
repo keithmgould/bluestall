8ffd2b4355e54f7d501756bb2adcd28fcb6b5d6b
Keith Gould <keithmgould@gmail.com>
2015-06-26 18:13:46 -0400

    trying to skip redis

--- bluestall.rb
+++ bluestall.rb
@@ -1,29 +1,10 @@
 require 'sinatra'
-require 'redis-sinatra'
+require 'pi_piper'
 
-register Sinatra::Cache
+include PiPiper
 
 get '/' do
-  return 'error' unless params['gender'] && params['floor']
-  return 'error' unless %w(men women).include?(params['gender'])
-
-  @status = settings.cache.fetch(build_key)
-
+  pin = PiPiper::Pin.new(pin: 7, direction: :in)
+  @status = pin.on? ? 'occupied' : 'vacant'
   erb :show
-end
-
-put '/' do
-  return 'error' unless params['gender'] && params['floor'] && params['status']
-  return 'error' unless %w(men women).include?(params['gender'])
-  return 'error' unless %w(occupied vacant).include?(params['status'])
-
-  settings.cache.write(build_key, params['status'])
-
-  params['status']
-end
-
-private
-
-def build_key
-  "#{params['gender']}.#{params['floor']}"
 end
